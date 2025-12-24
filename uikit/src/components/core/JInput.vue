9a61b55ff296096825f4dadfd1f88b1e58161b2f
SynkennL <Asyncleynn@hotmail.com>
2025-12-25 02:28:04 +0300

    Update

--- uikit/src/components/core/JInput.vue
+++ uikit/src/components/core/JInput.vue
@@ -54,23 +54,14 @@ const hasError = computed(() => !!props.error)
       <div v-if="icon" class="absolute left-3 top-1/2 -translate-y-1/2 text-slate-400">
         <slot name="icon" />
       </div>
-      <input
-        v-model="model"
-        :type="type"
-        :class="[
-          inputClasses, 
-          sizeClasses[size],
-          hasError ? 'border-red-400 focus:border-red-500 focus:ring-red-500/20' : '',
-          icon ? 'pl-10' : ''
-        ]"
-        :placeholder="placeholder"
-        :disabled="disabled"
-      />
+      <input v-model="model" :type="type" :class="[
+        inputClasses,
+        sizeClasses[size],
+        hasError ? 'border-red-400 focus:border-red-500 focus:ring-red-500/20' : '',
+        icon ? 'pl-10' : ''
+      ]" :placeholder="placeholder" :disabled="disabled" />
     </div>
     <span v-if="error" class="text-red-500 text-xs flex items-center gap-1">
-      <svg class="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
-        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
-      </svg>
       {{ error }}
     </span>
   </div>
